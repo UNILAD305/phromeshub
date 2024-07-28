083163092d7cdba65f4d41d5224d35c746e68c67
phromes <45674847+UNILAD305@users.noreply.github.com>
2024-07-28 06:38:36 -0400

    Update cmd-x.lua

--- cmd-x.lua
+++ cmd-x.lua
@@ -2,7 +2,7 @@
 while not game:IsLoaded() or not game:GetService("CoreGui") or not game:GetService("Players").LocalPlayer or not game:GetService("Players").LocalPlayer.PlayerGui do wait() end
 -- Constraints: -----------------------|
 local ver = "1.9.9f"
-local cordCode = "https://discord.gg/JyzvQ6zdfg"
+local cordCode = "https://discord.gg/xssntFpMX6"
 ---------------------------------------|
 Parents = {[1] = game:GetService("CoreGui"):FindFirstChild("RobloxGui"), [2] = game:GetService("CoreGui"), [3] = game:GetService("Players").LocalPlayer.PlayerGui}
 if Parents[1] then
